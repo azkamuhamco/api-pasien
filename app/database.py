406dd7d69e8bc7239b6de69468c9f54e81975daf
azkamuhamco <reprokbas@gmail.com>
2023-03-20 06:11:48 +0700

    docker python3.9 mariadb

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-engine   = create_engine("mysql+pymysql://root:@localhost:3306/zicare", pool_pre_ping=True, pool_recycle=300)
+engine   = create_engine("mariadb+mariadbconnector://root:@localhost:3306/zicare", pool_pre_ping=True, pool_recycle=300)
 Session  = sessionmaker(bind=engine)
 session  = scoped_session(Session)
